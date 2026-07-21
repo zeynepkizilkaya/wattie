30762c6e02a45d3f471fccffebf6160bb26daf56
Zeynep Kizilkaya <zeynepyildiz.kizilkaya@gmail.com>
2026-07-21 23:20:45 +0300

    Add scheduled consumption snapshot mechanism

--- src/main/java/com/i2i/wattie/WattieCoreApplication.java
+++ src/main/java/com/i2i/wattie/WattieCoreApplication.java
@@ -2,13 +2,13 @@ package com.i2i.wattie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class WattieCoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WattieCoreApplication.class, args);
 	}
-
 }
-
