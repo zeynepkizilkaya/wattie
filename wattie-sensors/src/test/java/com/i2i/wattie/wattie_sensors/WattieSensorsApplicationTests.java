060cdd8cd09bd74880ec200804ce2e5d53616dc0
zeynepsldrk <162623674+zeynepsldrk@users.noreply.github.com>
2026-07-22 13:23:31 +0300

    Fix WattieSensorsApplicationTests package name

--- wattie-sensors/src/test/java/com/i2i/wattie/wattie_sensors/WattieSensorsApplicationTests.java
+++ wattie-sensors/src/test/java/com/i2i/wattie/wattie_sensors/WattieSensorsApplicationTests.java
@@ -1,4 +1,4 @@
-package com.i2i.wattie.wattie_sensors;
+package com.i2i.wattie.sensors;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,3 +11,4 @@ class WattieSensorsApplicationTests {
 	}
 
 }
+
